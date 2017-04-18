44e9ac090928daaf2c1b4d2ab4852c349b6ed288
ysk_univ <ysk.univ.1007@gmail.com>
2017-04-18 22:34:57 +0900

    prompt

--- zshrc.sh
+++ zshrc.sh
@@ -10,3 +10,16 @@ if ! zgen saved; then
     # generate the init script from plugins above
     zgen save
 fi
+
+PROMPT='%B%n@%m:%F{cyan}[%~]%f%b
+%# '
+
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr     '%F{yellow}!%f'
+zstyle ':vcs_info:git:*' unstagedstr   '%F{red}+%f'
+zstyle ':vcs_info:*'     formats       '%F{blue}(%s)%f-%c%u%F{magenta}[%b]%f'
+zstyle ':vcs_info:*'     actionformats '%F{blue}(%s)%f-%F{magenta}[%b|%a]%f'
+precmd () { vcs_info }
+RPROMPT='%B${vcs_info_msg_0_}%b'
