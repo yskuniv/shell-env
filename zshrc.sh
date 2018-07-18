b1dd8b52618e10de1402d2a439dd8f9a66f5c5ac
ysk_univ <ysk.univ.1007@gmail.com>
2018-07-18 15:56:45 +0900

    fix to source zsh-prompt.sh

--- zshrc.sh
+++ zshrc.sh
@@ -7,19 +7,7 @@ source $SHELL_ENV_DIR/pyenv.sh
 source $SHELL_ENV_DIR/ssh-agent.sh
 
 source $SHELL_ENV_DIR/zgen.sh
-
-PROMPT='%B%n@%m:%F{cyan}[%~]%f%b
-%# '
-
-autoload -Uz vcs_info
-setopt prompt_subst
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr     '%F{yellow}!%f'
-zstyle ':vcs_info:git:*' unstagedstr   '%F{red}+%f'
-zstyle ':vcs_info:*'     formats       '%F{blue}(%s)%f-%c%u%F{magenta}[%b]%f'
-zstyle ':vcs_info:*'     actionformats '%F{blue}(%s)%f-%F{magenta}[%b|%a]%f'
-precmd () { vcs_info }
-RPROMPT='%B${vcs_info_msg_0_}%b'
+source $SHELL_ENV_DIR/zsh-prompt.sh
 
 # history
 export HISTFILE=${HOME}/.zsh_history
