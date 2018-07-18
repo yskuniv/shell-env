7ddb0fbf642c9bd83b53068817a14b9821157733
ysk_univ <ysk.univ.1007@gmail.com>
2018-07-18 16:02:01 +0900

    fix filepaths to source

--- zshrc.sh
+++ zshrc.sh
@@ -1,13 +1,10 @@
 SHELL_ENV_DIR=$(cd `dirname $0` && pwd -P)
 
 
-source $SHELL_ENV_DIR/alias.sh
-source $SHELL_ENV_DIR/rbenv.sh
-source $SHELL_ENV_DIR/pyenv.sh
-source $SHELL_ENV_DIR/ssh-agent.sh
+source $SHELL_ENV_DIR/common.sh
 
-source $SHELL_ENV_DIR/zgen.sh
-source $SHELL_ENV_DIR/zsh-prompt.sh
+source $SHELL_ENV_DIR/zsh/zgen.sh
+source $SHELL_ENV_DIR/zsh/prompt.sh
 
 # history
 export HISTFILE=${HOME}/.zsh_history
