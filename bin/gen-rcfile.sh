c1ad2f29d966e0a8e0ace10df4fa15a3a50d0f20
ysk_univ <ysk.univ.1007@gmail.com>
2019-10-22 20:23:00 +0900

    add some fix into gen-rcfile.sh

--- bin/gen-rcfile.sh
+++ bin/gen-rcfile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-shell_name=$1
+target_shell=$1
 shell_env_dir=$(cd `dirname $0` && pwd -P)
 
+if [ "x$target_shell" = x ]; then
+    echo "Error: target shell is not specified" >&2
+    exit 1
+fi
+
+
 cat <<EOF
-source ${shell_env_dir}/${shell_name}rc.sh
+source ${shell_env_dir}/${target_shell}rc.sh
 EOF
