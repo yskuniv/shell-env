f528eaf36466cdc8f081dfbe10e32750b4096dfe
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-28 22:23:18 +0900

    delete common.sh

--- common.sh
+++
@@ -1,6 +0,0 @@
-source $SHELL_ENV_DIR/common/alias.sh
-source $SHELL_ENV_DIR/common/rbenv.sh
-source $SHELL_ENV_DIR/common/pyenv.sh
-source $SHELL_ENV_DIR/common/ssh-agent.sh
-
-[ -f $SHELL_ENV_DIR/local/common.sh ] && source $SHELL_ENV_DIR/local/common.sh
