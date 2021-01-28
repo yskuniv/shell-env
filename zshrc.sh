4eb16baee20647795d47dcaef363b748dfdf07a6
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-28 22:28:02 +0900

    [zshrc.sh] fix

--- zshrc.sh
+++ zshrc.sh
@@ -1,15 +1,4 @@
 SHELL_ENV_DIR=$(cd `dirname $0` && pwd -P)
 
 
-source $SHELL_ENV_DIR/common.sh
-
-source $SHELL_ENV_DIR/zsh/zgen.sh
-source $SHELL_ENV_DIR/zsh/prompt.sh
-source $SHELL_ENV_DIR/zsh/iterm2.sh
-
-# history
-export HISTFILE=${HOME}/.zsh_history
-export HISTSIZE=1000
-export SAVEHIST=10000
-setopt hist_ignore_dups
-setopt EXTENDED_HISTORY
+source $SHELL_ENV_DIR/zsh/init.sh
