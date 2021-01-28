605edcfefffe4e40c9b17639c9555e8eee94140a
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-28 22:25:55 +0900

    add zsh/{init,history}.sh

---
+++ zsh/init.sh
@@ -0,0 +1,7 @@
+source $SHELL_ENV_DIR/common/init.sh
+
+source $SHELL_ENV_DIR/lib/zsh/zgen.sh
+source $SHELL_ENV_DIR/lib/zsh/prompt.sh
+source $SHELL_ENV_DIR/lib/zsh/iterm2.sh
+
+source $SHELL_ENV_DIR/zsh/history.sh
