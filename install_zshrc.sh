8313dfb488b2184c3ea32bd9059f334068aab851
ysk <ysk.univ.1007@gmail.com>
2023-10-12 08:27:18 +0900

    add install scripts

---
+++ install_zshrc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ask_yes_no () {
+    msg="$1"
+
+    echo -n "${msg} [Y/n]: "
+
+    read ans
+
+    case $ans in
+        "[Yy]" | "[Yy]es" | "" )
+            return 0
+            ;;
+
+        * )
+            return 1
+            ;;
+    esac
+}
+
+
+if ask_yes_no "CAUTION: This script will break current '.zshrc' if it exists. Is it OK?"; then
+    cat <<"EOF" > "${HOME}/.zshrc"
+SHELL_ENV="${HOME}/env/shell-env"
+
+source "${SHELL_ENV}/zshrc.sh"
+EOF
+fi
